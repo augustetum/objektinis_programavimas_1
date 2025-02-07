b90ced9b09859f2983633ee34bc84068c04bcea7
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2025-02-07 10:53:06 +0200

    Add standard libraries and student structure

---
+++ mano_lib.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+
+struct Studentas{
+    string vardas, pavarde;
+    vector<int> pazymiai;
+    int egzaminas;
+};
