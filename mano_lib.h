66db67efb041ab56e38de492ffe6678aadb5d3d6
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2025-02-10 16:36:50 +0200

    Add final grade calculation with median
    Fixed grade calculation and moved student initialization to the inside of the while loop, as it was causing grades to accumulate in the same vector instead of separate ones for each student

--- mano_lib.h
+++ mano_lib.h
@@ -16,3 +16,24 @@ struct Studentas{
     vector<int> pazymiai;
     int egzaminas;
 };
+
+double skaiciuotiGalutiniSuVidurkiu(Studentas stud){
+    double sum = accumulate(stud.pazymiai.begin(), stud.pazymiai.end(),0);
+    double pazymiuVidurkis = sum / (double)stud.pazymiai.size();
+    double galutinis = 0.4 * pazymiuVidurkis + 0.6 * stud.egzaminas;
+    return galutinis;
+}
+
+double skaiciuotiGalutiniSuMediana(Studentas stud){
+    int n = stud.pazymiai.size();
+    int med = 1;
+    sort(stud.pazymiai.begin(), stud.pazymiai.end());
+    if ( n % 2 != 0){
+        med = (double)stud.pazymiai[n/2];
+    } else {
+        med = (double)(stud.pazymiai[(n-1)/2] + stud.pazymiai[n/2]) / 2.0;
+    }
+
+    double galutinis = 0.4 * med + 0.6 * stud.egzaminas;
+    return galutinis;
+}
--- programa.cpp
+++ programa.cpp
@@ -1,11 +1,9 @@
 #include "mano_lib.h"
 
-double skaiciuotiGalutini(Studentas stud);
-
 int main(){
-    Studentas stud;
     vector<Studentas> studentuSarasas;
     while (true){
+            Studentas stud;
             cout << "Įveskite studento vardą (įvedę visus norimus studentus ir norėdami užbaigti rašykite stop)" << endl;
             cin >> stud.vardas;
             if (stud.vardas == "stop"){
@@ -27,17 +25,29 @@ int main(){
             studentuSarasas.push_back(stud);
     }
 
-    cout << std::left << std::setw(20) << "Pavardė" << std::setw(20) << "Vardas" << std::setw(20) << "Galutinis (Vid.)" << endl;
-    cout << "-------------------------------------" << endl;
-    for (Studentas s : studentuSarasas){
-        cout << std::left << std::setw(20) << s.pavarde << std::setw(20) << s.vardas << std::setw(20) << skaiciuotiGalutini(s) << endl;
+    int choice;
+    cout << "Jei norėtumete galutinį balą skaičiuoti su vidurkiu, įrašykite 0, jei su mediana - įrašykite 1";
+    cin >> choice;
+
+    cout << "Štai rezultatai: " << endl;
+    cout << endl;
+
+    cout << std::left << std::setw(20) << "Pavardė" << std::setw(20) << "Vardas";
+
+    if (choice == 0){
+        cout << std::setw(20) << "Galutinis (Vid.)" << endl;
+        cout << "---------------------------------------------------------" << endl;
+        for (Studentas s : studentuSarasas){
+            cout << std::left << std::setw(20) << s.pavarde << std::setw(20) << s.vardas << std::setw(20) << std::fixed << std::setprecision(2) << skaiciuotiGalutiniSuVidurkiu(s) << endl;
+        }
+    } else if (choice == 1){
+        cout << std::setw(20) << "Galutinis (Med.)" << endl;
+        cout << "---------------------------------------------------------" << endl;
+        for (Studentas s : studentuSarasas){
+            cout << std::left << std::setw(20) << s.pavarde << std::setw(20) << s.vardas << std::setw(20) << std::fixed << std::setprecision(2) << skaiciuotiGalutiniSuVidurkiu(s) << endl;
+        }
+    } else {
+        cout << "Neteisingai įvestas pasirinkimas";
     }
 
-}
-
-double skaiciuotiGalutini(Studentas stud){
-    int sum = accumulate(stud.pazymiai.begin(), stud.pazymiai.end(),0);
-    double pazymiuVidurkis = sum / (double)stud.pazymiai.size(); //pataisyta
-    double galutinis = 0.4 * pazymiuVidurkis + 0.6 * stud.egzaminas;
-    return galutinis;
 }
